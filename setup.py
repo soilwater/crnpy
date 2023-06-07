22f99ca4f74128d17235dc0efa6fa98fedb8e831
Joaquin Peraza <joaquin@peraza.uy>
2023-06-07 10:03:00 -0500

    add sample test

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="crnpy",
-    version="0.2-28",
+    version="0.2-29",
     packages=['crnpy'],
     package_dir = {"": "src"},
     include_package_data=True,
