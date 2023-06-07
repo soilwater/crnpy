541209b4c842f17f0c98c0b368287f5bdda2ca9d
Joaquin Peraza <joaquin@peraza.uy>
2023-06-07 10:15:27 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,4 +7,6 @@ setuptools.setup(
     packages=['crnpy'],
     package_dir = {"": "src"},
     include_package_data=True,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
 )
