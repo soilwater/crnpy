2afbf12f4d2d4f2b41bf3383c93bd6d132f5844a
Joaquin Peraza <joaquin@peraza.uy>
2023-06-19 07:28:29 -0500

    fix readme

--- setup.py
+++ setup.py
@@ -3,10 +3,15 @@ import setuptools
 
 setuptools.setup(
     name="crnpy",
-    version="0.3-1",
+    version="0.3.1",
     packages=['crnpy'],
     package_dir = {"": "src"},
+    description="A Python package for processing cosmic ray neutron data.",
     include_package_data=True,
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
+    url="https://github.com/soilwater/crnpy",
+    author="Joaquin Peraza, Andres Patrignani",
+    author_email="jperaza@ksu.edu, andrespatrignani@ksu.edu",
+    license="MIT"
 )
