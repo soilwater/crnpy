d18f17e74526f1f359d8df5bae4436570f6ae4bd
Joaquin Peraza <joaquin@peraza.uy>
2023-06-20 10:06:37 -0500

    site changes

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     version="0.3.1",
     packages=['crnpy'],
     package_dir = {"": "src"},
-    description="A Python package for processing cosmic ray neutron data.",
+    description="A Python package for the estimation and processing of soil moisture data from cosmic-ray neutron counts.",
     include_package_data=True,
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
