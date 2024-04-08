Fixed Weighting according to Schrön et al. (2017)
Addressing the comments in #9, the method was updated to use the iterative routine.

Examples and tests, were also updated.

src/crnpy/crnpy.py
@@ -706,7 +706,7 @@ def abs_humidity(relative_humidity, temp):
     return abs_h
 
 
-def nrad_weight(h, theta, distances, depth, rhob=1.4, method="Kohli_2015", p=None, Hveg=None):
+def nrad_weight(h, theta, distances, depth, rhob=1.4, method="Kohli_2015", p=None, Hveg=0, tol=0.01):
     """Function to compute distance weights corresponding to each soil sample.
 
     Args:
@@ -718,9 +718,11 @@ def nrad_weight(h, theta, distances, depth, rhob=1.4, method="Kohli_2015", p=Non
         p (np.array or pd.Series): Atmospheric pressure in hPa. Required for the 'Schron_2017' method.
         Hveg (np.array or pd.Series): Vegetation height in m. Required for the 'Schron_2017' method.
         method (str): Method to compute the distance weights. Options are 'Kohli_2015' or 'Schron_2017'.
+        tol (float): Tolerance for the iterative solution. Default is 0.01. Required for the 'Schron_2017' method.
 
     Returns:
-        (array or pd.Series or pd.DataFrame): Distance weights for each sample.
+        theta_new (np.array or pd.Series): Weighted soil moisture values.
+        weights (np.array or pd.Series): Distance weights for each sample. For the 'Schron_2017' method, the weights are computed for each distance.
 
     References:
         Köhli, M., Schrön, M., Zreda, M., Schmidt, U., Dietrich, P., and Zacharias, S. (2015).
@@ -826,7 +828,10 @@ def nrad_weight(h, theta, distances, depth, rhob=1.4, method="Kohli_2015", p=Non
 
         # Combined and normalized weights
         weights = Wd * W / np.nansum(Wd * W)
-        return weights
+
+        theta_new = np.sum(theta * weights)
+
+        return theta_new, weights
     elif method == 'Schron_2017':
         # Horizontal distance weights According to Eq. 6 and Table A1 in Schrön et al. (2017)
         # Method for calculating the horizontal distance weights from 0 to 1m
@@ -929,6 +934,17 @@ def nrad_weight(h, theta, distances, depth, rhob=1.4, method="Kohli_2015", p=Non
 
             return B0 * np.exp(-B1 * r) + B2 * np.exp(-B3 * r)
 
+        # Wrapper method for calculating the horizontal distance weights
+        def Wr(r, x, y):
+            if r <= 1:
+                return WrX(r, x, y)
+            elif r <= 50:
+                return WrA(r, x, y)
+            elif r <= 600:
+                return WrB(r, x, y)
+            else:
+                raise ValueError("r must be between 1 and 600m when using 'Schron_2017' method")
+
         def rscaled(r, p, y, Hveg = 0):
             Fp = 0.4922 / (0.86 - np.exp(-p / 1013.25))
             Fveg = 1 - 0.17 * (1 - np.exp(-0.41 * Hveg)) * (1 + np.exp(-9.25 * y))
@@ -936,37 +952,52 @@ def nrad_weight(h, theta, distances, depth, rhob=1.4, method="Kohli_2015", p=Non
 
         # Rename variables to be consistent with the revised paper
         r = distances
-        x = h
-        y = theta
-        bd = rhob
+        theta_ = np.mean(theta) # Start with the mean value of theta as initial guess
+        bd = np.mean(rhob) # Neutrons are impacted by the bulk density across the whole area and not just the sample area. https://github.com/soilwater/crnpy/issues/9#issuecomment-2003813777
 
-        r = rscaled(r, p, y, Hveg)
-
-        Wr = np.zeros(len(r))
-
-        # See Eq. 6 in Schron et al. (2017)
-        r0_idx = (r <= 1)
-        r1_idx = (r > 1) & (r <= 50)
-        r2_idx = (r > 50) & (r < 600)
-        Wr[r0_idx] = WrX(r[r0_idx], x[r0_idx], y[r0_idx])
-        Wr[r1_idx] = WrA(r[r1_idx], x[r1_idx], y[r1_idx])
-        Wr[r2_idx] = WrB(r[r2_idx], x[r2_idx], y[r2_idx])
-
-        # Vertical distance weights
+        # Vertical distance weights functions
         def D86(r, bd, y):
             return 1 / bd * (8.321 + 0.14249 * (0.96655 + np.exp(-0.01 * r)) * (20 + y) / (0.0429 + y))
 
         def Wd(d, r, bd, y):
             return np.exp(-2 * d / D86(r, bd, y))
 
-        # Calculate the vertical distance weights
-        Wd = Wd(depth, r, bd, y)
+        step = 0
+        diff = 1
+        while diff > tol:
+            step += 1
+            print(f"Step {step}, diff = {diff}", end="\r")
+            # Calculate the scaled distance and D86
+            r = rscaled(distances, p, theta_, Hveg)
 
-        # Combined and normalized weights
-        # Combined and normalized weights
-        weights = Wd * Wr / np.nansum(Wd * Wr)
+            # Calculate the vertical average for each profile
+            P = np.unique(distances)
+            theta_P = []
+            r_stars = []
+            for i in range(len(P)):
+                profile = P[i]
+                idx = distances == profile
+                depths_P = depth[idx]
+                r_P = r[idx]
+                theta_Pi = theta[idx]
+                # Calculate the vertical distance weights
+                Wd_P = Wd(depths_P, r_P, bd, theta_Pi)
+                # Calculate the vertical average of theta
+                theta_P_i = np.sum(Wd_P * theta_Pi) / np.sum(Wd_P)
+                theta_P.append(theta_P_i)
+                r_stars.append(np.mean(r_P))
 
-        return weights
+            # Calculate the horizontal distance weights
+            Wrs = np.array([Wr(r_star, theta_p, Hveg) for r_star, theta_p in zip(r_stars, theta_P)])
+            theta_new = np.sum(Wrs * theta_P) / np.sum(Wrs)
+            diff = np.abs(theta_new - theta_)
+            theta_ = theta_new
+
+        print(f"Solution converged after {step} steps, the average soil moisture is {theta_new}")
+
+        return theta_new, [theta_P, r_stars, Wrs]
+    else:
+        raise ValueError(f"Method {method} not recognized. Please use one of the following methods: 'Kohli_2015' or 'Schron_2017'")
 
 
 def exp_filter(sm, T=1):

src/tests/test_calibration_rdt.py
@@ -62,25 +62,30 @@ def calibration_example():
                 df_station['fp'])
 
     # Compute the weights of each sample for the field average
-    nrad_weights = crnpy.nrad_weight(df_station['abs_humidity'].mean(), df_soil['theta_v'],
-                                     df_soil['distance_from_station'],
-                                     (df_soil['bottom_depth'] + df_soil['top_depth']) / 2,
-                                     rhob=df_soil['bulk_density'].mean())
+    field_theta_v_1, w = crnpy.nrad_weight(df_station['abs_humidity'].mean(), df_soil['theta_v'],
+                                         df_soil['distance_from_station'],
+                                         (df_soil['bottom_depth'] + df_soil['top_depth']) / 2,
+                                         rhob=df_soil['bulk_density'].mean(), method="Kohli_2015")
+
+    field_theta_v_2, w = crnpy.nrad_weight(df_station['abs_humidity'].mean(), df_soil['theta_v'],
+                                         df_soil['distance_from_station'],
+                                         (df_soil['bottom_depth'] + df_soil['top_depth']) / 2,
+                                         rhob=df_soil['bulk_density'].mean(),
+                                         p=df_station['barometric_pressure_Avg'].mean(), method="Schron_2017")
 
     # Apply distance weights to volumetric water content and bulk density
-    field_theta_v = np.sum(df_soil['theta_v'] * nrad_weights)
-    field_bulk_density = np.sum(df_soil['bulk_density'] * nrad_weights)
+    field_bulk_density = np.sum(df_soil['bulk_density'].mean())
 
     # Determine the mean corrected counts during the calibration survey
     idx_cal_period = (df_station['TIMESTAMP'] >= calibration_start) & (df_station['TIMESTAMP'] <= calibration_end)
     mean_cal_counts = df_station.loc[idx_cal_period, 'total_corrected_neutrons'].mean()
 
-    print(f"Mean volumetric Water content during calibration survey: {round(field_theta_v, 3)}")
+    print(f"Mean volumetric Water content during calibration survey: {round(field_theta_v_2, 3)}")
     print(f"Mean corrected counts during calibration: {round(mean_cal_counts)} counts")
 
     # Define the function for which we want to find the roots
     VWC_func = lambda N0: crnpy.counts_to_vwc(mean_cal_counts, N0, bulk_density=field_bulk_density, Wlat=0.03,
-                                              Wsoc=0.01) - field_theta_v
+                                              Wsoc=0.01) - field_theta_v_1
 
     # Make an initial guess for N0
     N0_initial_guess = 1000
@@ -91,9 +96,28 @@ def calibration_example():
     # Print the solution
     print(f"The solved value for N0 is: {sol}")
 
-    return sol
+    # Compute close and far soil moisture to compare both weight methods
+    close_sm = df_soil[(df_soil['distance_from_station'] < 10) & (df_soil['bottom_depth'] < 10)]['theta_v'].mean()
+    far_sm = df_soil[(df_soil['distance_from_station'] > 10) & (df_soil['bottom_depth'] < 10)]['theta_v'].mean()
+    return sol, field_theta_v_1, field_theta_v_2, close_sm, far_sm
 
 def test_calibration():
-    solved_N0 = calibration_example()
+    solved_N0, field_sm_Kohli, field_sm_Schron, close_sm, far_sm  = calibration_example()
     print("The expected value for N0 is between 2500 and 2800")
+
     assert solved_N0 > 2500 and solved_N0 < 2800, f"Calibration test failed. Solved N0: {solved_N0}, expected value between 2500 and 2800."
+    print("The expected value for field soil moisture using Kohli et al. (2015) or Schron et al. (2017) is between 0.25 and 0.30")
+    assert field_sm_Kohli > 0.25 and field_sm_Kohli < 0.30, f"Calibration test failed. Field soil moisture using Kohli et al. (2015): {field_sm_Kohli}, expected value between 0.25 and 0.30."
+    assert field_sm_Schron > 0.2 and field_sm_Schron < 0.30, f"Calibration test failed. Field soil moisture using Schron et al. (2017): {field_sm_Schron}, expected value between 0.25 and 0.30."
+
+    print(f"Close soil moisture: {close_sm}")
+    print(f"Far soil moisture: {far_sm}")
+    print(f"Field soil moisture using Kohli et al. (2015): {field_sm_Kohli}")
+    print(f"Field soil moisture using Schron et al. (2017): {field_sm_Schron}")
+
+    # Schron et al. (2017) method should more influenced by close soil moisture samples according to CRNP sensitivity assumptions
+
+    dif_Kohli = abs(field_sm_Kohli - close_sm)
+    dif_Schron = abs(field_sm_Schron - close_sm)
+
+    assert dif_Schron < dif_Kohli, f"Calibration test failed. Schron et al. (2017) method should be more influenced by close soil moisture samples according to CRNP sensitivity assumptions."

src/tests/test_functions.py
@@ -45,27 +45,18 @@ def test_weighting():
     y = np.repeat(0, len(x))
     h = np.repeat(10, len(x))
 
-
     p = np.repeat(1013.25, len(x))
     Hveg = np.repeat(0, len(x))
     bd = 1.1
     SM = 0.02
     sm = np.repeat(SM, len(x))
 
-    # calculate weighting function using Schrön et al. 2017 method
-    weights = crnpy.nrad_weight(h, sm, x, y, bd, method="Schron_2017", p=p, Hveg=Hveg)
-    # check that the sum of the weights is 1
-    Max = 0.01147
-    Avg = 0.00029
-    Min = 0.00000
-    assert round(np.sum(weights),5) == 1
-    # Compare to the values in supplementary material of Schrön et al. 2017
-    # check that the max weight is 0.01
-    assert round(np.max(weights),3) == round(Max,3)
-    # check that the avg weight is 0.00029
-    assert round(np.mean(weights),5) == round(Avg,5)
-    # check that the min weight is 0.00000
-    assert round(np.min(weights),5) == round(Min,5)
+    # calculate the weighted theta
+    theta_adj, w = crnpy.nrad_weight(h, sm, x, y, bd, method="Schron_2017", p=p, Hveg=Hveg)
+
+    # check that theta_adj is = SM +- 1e-3
+    assert np.abs(theta_adj - SM) < 1e-3
+
 
 def test_correction_inncoming_neutrons_RcMethods():
     # Test Hawdon 2014 Method using Baldry location from the original paper